e54812d30a8a70bd6348261027934e438f55fa0d
liujiangtao <1731473343@qq.com>
2024-05-30 13:07:32 +0800

    optimized history record

--- src/main/java/com/VIVO/echotale/utils/FileUtil.java
+++ src/main/java/com/VIVO/echotale/utils/FileUtil.java
@@ -3,8 +3,13 @@ package com.VIVO.echotale.utils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,12 +21,22 @@ public class FileUtil {
     public static List<Map<String, String>> loadMessages() throws IOException {
         File file = new File(FILE_PATH);
         if (!file.exists()) {
-            return null;
+            return new ArrayList<>();
         }
-        return objectMapper.readValue(file, new TypeReference<List<Map<String, String>>>() {});
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        reader.close();
+        return objectMapper.readValue(sb.toString(), new TypeReference<List<Map<String, String>>>() {});
     }
 
     public static void saveMessages(List<Map<String, String>> messages) throws IOException {
-        objectMapper.writeValue(new File(FILE_PATH), messages);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH));
+        String jsonString = objectMapper.writeValueAsString(messages);
+        writer.write(jsonString.replace("},{", "},\n{")); // 插入换行符
+        writer.close();
     }
 }
